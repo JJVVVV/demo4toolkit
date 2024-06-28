685cff9620c4e79ab09c69eb35bcbf6bc12b5b1d
jiah-li <lijiahuiim@gmail.com>
2024-06-29 00:21:56 +0800

    .

--- utils/evaluators.py
+++ utils/evaluators.py
@@ -17,7 +17,7 @@ from torch.utils.data import DataLoader
 from tqdm.auto import tqdm
 
 
-class Evaluator1(Evaluator):
+class Evaluator4Generate(Evaluator):
     write_file = True
     input_output_label_path = "input_output_label.json"
 
@@ -44,6 +44,45 @@ class Evaluator1(Evaluator):
                 with open(generate_result_path, "w") as f:
                     json.dump(dict_list, f, indent=2, ensure_ascii=False)
             metric = rouge([it["pred"] for it in dict_list], [it["label"] for it in dict_list], "en", ("rougeL")) * 100
+            metric.round(2)
+            objects4sync.append(metric)
+        else:
+            objects4sync = [None]
+        if world_size > 1:
+            dist.broadcast_object_list(objects4sync, src=0)
+        return objects4sync[0]
+
+
+class Evaluator4Classify(Evaluator):
+    write_file = True
+    input_output_label_path = "input_output_label.json"
+
+    def calculate_metric_callback(self, all_labels: list[str], all_logits: list[str], mean_loss: float) -> MetricDict:
+        local_rank = dist.get_rank() if dist.is_initialized() else 0
+        world_size = dist.get_world_size() if dist.is_initialized() else 1
+
+        if local_rank == 0:
+            objects4sync = []
+            dict_list = []
+            for idx, (a_label, a_logits) in enumerate(zip(all_labels, all_logits)):
+                a = {"idx": idx, "label": a_label, "pred": a_logits}
+                dict_list.append(a)
+            dict_list = sorted(dict_list, key=lambda x: x["idx"])
+            if self.write_file:
+                generate_result_path: Path = (
+                    self.config.save_dir
+                    / "evaluators"
+                    / f"epoch={self.config.training_runtime['cur_epoch']:03d}_step={self.config.training_runtime['cur_step']}"
+                    / self.split.name
+                    / self.input_output_label_path
+                )
+                generate_result_path.parent.mkdir(parents=True, exist_ok=True)
+                with open(generate_result_path, "w") as f:
+                    json.dump(dict_list, f, indent=2, ensure_ascii=False)
+            all_preds = (np.argmax(all_logits, axis=1, keepdims=True) >= 50).astype(int)
+            acc = accuracy_score(all_labels, all_preds)
+            f1 = f1_score(all_labels, all_preds, average="binary")
+            metric = MetricDict({"accuracy": acc * 100, "F1-score": f1 * 100, "loss": mean_loss})
             metric.round(2)
             objects4sync.append(metric)
         else:
--- utils/load_data_fn.py
+++ utils/load_data_fn.py
@@ -16,7 +16,7 @@ class TextType(Enum):
     ORI = auto()
 
 
-def load_data_fn(data_file_path: Path | str, model_type: str, tokenizer: PreTrainedTokenizer, split: Split, **kwargs):
+def load_data_fn4generate(data_file_path: Path | str, model_type: str, tokenizer: PreTrainedTokenizer, split: Split, **kwargs):
     special_tokens_map = tokenizer.special_tokens_map
     BOS = special_tokens_map["bos_token"] if "bos_token" in special_tokens_map.keys() else None
     EOS = special_tokens_map["eos_token"] if "eos_token" in special_tokens_map.keys() else None
@@ -57,12 +57,12 @@ def load_data_fn(data_file_path: Path | str, model_type: str, tokenizer: PreTrai
         # input
         match text_type:
             case TextType.ORI:
-                input_str = ""
+                input_str = row["inputs"]
                 a_sample = PairedText(input_str)
         # label
         match text_type:
-            case TextType.LCQ:
-                label_str = ""
+            case TextType.ORI:
+                label_str = row["outputs"]
                 if split == Split.TRAINING:
                     a_label = PairedText(label_str + EOS)
                 else:
@@ -70,5 +70,61 @@ def load_data_fn(data_file_path: Path | str, model_type: str, tokenizer: PreTrai
         inputs.append(a_sample)
         labels.append(a_label)
 
-        print(f"### Input ###\n{input_str}\n\n### Output ###\n{label_str}\n\n")
+    print(f"### Input ###\n{input_str}\n\n### Output ###\n{label_str}\n\n")
+    return inputs, labels
+
+
+def load_data_fn4classify(data_file_path: Path | str, model_type: str, tokenizer: PreTrainedTokenizer, split: Split, **kwargs):
+    special_tokens_map = tokenizer.special_tokens_map
+    BOS = special_tokens_map["bos_token"] if "bos_token" in special_tokens_map.keys() else None
+    EOS = special_tokens_map["eos_token"] if "eos_token" in special_tokens_map.keys() else None
+    SEP = special_tokens_map["sep_token"] if "sep_token" in special_tokens_map.keys() else None
+    MASK = special_tokens_map["mask_token"] if "mask_token" in special_tokens_map.keys() else None
+    CLS = special_tokens_map["cls_token"] if "cls_token" in special_tokens_map.keys() else None
+    text_type = TextType[kwargs["text_type"]]
+    try:
+        model_name = kwargs["model_name"]
+    except:
+        model_name = None
+
+    # t2s_ratio = re.search(r"mix_t2s_ratio=([\d\.]+)", model_name)
+    # if t2s_ratio:
+    #     t2s_ratio = float(t2s_ratio.group(1))
+    # no_error_ratio = re.search(r"mix_no_error_ratio=([\d\.]+)", model_name)
+    # if no_error_ratio:
+    #     no_error_ratio = float(no_error_ratio.group(1))
+
+    if isinstance(data_file_path, str | Path):
+        data_file_path = Path(data_file_path)
+        if data_file_path.is_dir():
+            dfs = [pd.read_json(p) for p in data_file_path.iterdir()]
+            iterator = chain(*[pd.read_json(p).iterrows() for p in data_file_path.iterdir()])
+            n = sum((len(df) for df in dfs))
+        else:
+            df = pd.read_json(data_file_path, lines=False)
+            iterator = df.iterrows()
+            n = len(df)
+    else:
+        n = len(data_file_path)
+        iterator = data_file_path.iterrows()
+
+    inputs = []
+    labels = []
+    print(df.columns)
+    for idx, row in iterator:
+        # input
+        match text_type:
+            case TextType.ORI:
+                input_str = row["inputs"]
+                a_sample = PairedText(input_str)
+        # label
+        match text_type:
+            case TextType.ORI:
+                label_str = row["outputs"]
+                a_label = ClassificationLabel(label_str)
+
+        inputs.append(a_sample)
+        labels.append(a_label)
+
+    print(f"### Input ###\n{input_str}\n\n### Output ###\n{label_str}\n\n")
     return inputs, labels
